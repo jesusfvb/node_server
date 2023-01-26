fc99fd6f37bea3236779fae4c46c7af195233dff
Jesús Francisco Vázquez Biltre <jesusfvb2@gmail.com>
2023-01-25 23:57:58 -0500

    Validando parámetros del body

--- src/user/controller.ts
+++ src/user/controller.ts
@@ -2,12 +2,18 @@ import { Request, Response } from "express";
 import bcryptjs from "bcryptjs";
 
 import UserModel, { User } from "./model";
+import { validationResult } from "express-validator";
 
 export const list = async (req: Request, res: Response) => {
   res.json(await UserModel.find());
 };
 
 export const save = async (req: Request<{}, {}, User>, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors);
+  }
 
   if (!!(await UserModel.findOne({ email: req.body.email }))) {
     return res.json({
@@ -19,6 +25,7 @@ export const save = async (req: Request<{}, {}, User>, res: Response) => {
       message: "Username exist",
     });
   }
+
   let user = new UserModel(req.body);
 
   const salt = bcryptjs.genSaltSync(10);
--- src/user/router.ts
+++ src/user/router.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
 import { list, save } from "./controller";
+import { body } from "express-validator";
 
 const router = Router();
 
 router.get("/", list);
 
-router.post("/", save);
+router.post(
+  "/",
+  [
+    body("email", "Email is required").isEmail(),
+    body("username", "Username is required").not().isEmpty(),
+  ],
+  save
+);
 export default router;
